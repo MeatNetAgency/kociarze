9dc3bbdb88c3a5d8e39e203ba1e8453edaa796a8
Karol Sobolewski <karol@meatnet.pl>
2015-10-20 11:36:41 +0200

    Pierwszy komit
    wszystko

---
+++ js/main.js
@@ -0,0 +1,75 @@
+$(document).ready(function()
+{
+	
+	//slider
+	$('.flexslider').flexslider(
+	{
+		animation: "slide"
+	});
+	
+	
+	
+	
+	// set up slider
+	$("#main-slider").css("position", "absolute").css("top", 0).css("z-index", "-2");
+	
+	
+	
+	
+	
+	
+	
+	
+	//resize main-menu row:
+	var height = 				$("#main-menu").outerHeight();
+	var width = 				$("#main-menu").width();
+	var marginRight = 		$("#main-menu").css("margin-right");
+	
+	var spacerWidth = $("#main-menu").parent().width() - ($("#main-menu").width() + $("#search-box").width());
+	
+	//alert($("#main-menu").width() + "\n" + $("#search-box").width() + "\n" + $("#main-menu").parent().width());
+	
+	$("#menu-spacer").css("margin-left", (width-1)).css("width", spacerWidth);
+	
+	var position = $("#main-menu").position();
+	var lenght = position.left;
+	$("#row-spacer").css("width", lenght);
+	
+	$( window ).resize(function() {
+		var height = 				$("#main-menu").outerHeight();
+		var width = 				$("#main-menu").width();
+		var marginRight = 		$("#main-menu").css("margin-right");
+		var spacerWidth = $("#main-menu").parent().width() - ($("#main-menu").width() + $("#search-box").width());
+		$("#menu-spacer").css("margin-left", (width-1)).css("width", spacerWidth);
+		
+		var position = $("#main-menu").position();
+		var lenght = position.left;
+		$("#row-spacer").css("width", lenght);
+	});
+	
+	
+	
+	
+	
+	
+	
+	// menu hover borders
+	$("#main-menu ul li").hover(function() {
+			var width = $(this).outerWidth();
+			var position = $(this).position();
+			var left = position.left;
+			
+			$("#nav-border-top").css("width", width).css("left", left).css("z-index", "90").show();
+			$("#nav-border-bottom").css("width", width).css("left", left).css("z-index", "90").show();
+			
+	}, function() {
+		$("#nav-border-top").hide();
+		$("#nav-border-bottom").hide();
+	});
+	
+	
+	
+	
+	
+	
+}); 
---
+++ test.php
@@ -0,0 +1,80 @@
+﻿<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Kociarze.pl</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!--JS JQUERY-->
+        <script src="js/jquery-1.11.3.min.js"></script>
+        
+        <!--JS JQUERY UI-->
+        <script src="js/jquery-ui/jquery-ui.min.js"></script>
+        
+        <!--JS FLEXSLIDER2-->
+        <script src="js/flexslider/jquery.flexslider-min.js"></script>
+
+        <!--MOSAIC FLOW PHOTO-->
+        <!--<script src="js/jquery.mosaicflow.min.js"></script>-->
+        
+        <!--JS MAIN-->
+        <script src="js/main.js"></script>
+            
+        <!--CSS GRID 1280 PX-->
+        <link rel="Stylesheet" type="text/css" href="css/grid.css" />
+        
+        <!-- GOOGLE FONTS: LATO -->
+        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,100&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        
+        <!--CSS FLEXSLIDER2-->
+        <link rel="Stylesheet" type="text/css" href="js/flexslider/flexslider.css" />
+        
+        <!--CSS JQUERY UI-->
+        <link rel="Stylesheet" type="text/css" href="js/jquery-ui/jquery-ui.min.css" />
+        
+        <!-- MAIN CSS -->
+        <link rel="Stylesheet" type="text/css" href="css/main.css" />
+
+	</head>
+        
+	<body>
+    
+    
+    
+    
+    
+    
+  
+    
+    
+    <!--SLIDER-->
+        <div id="main-slider">
+            <div class="flexslider">
+                <ul class="slides">
+                
+                	<!--FIRST SLIDER-->
+                    <li><img src="img/layer-main.png" alt="slide1" /></li>
+                    
+                    <!--SECOND SLIDER-->
+                    <li><img src="img/layer-main.png" alt="slide2" /></li>  
+                </ul>
+            </div>
+		</div>
+       
+       
+       
+       
+       
+       <div class="container">
+       		<div class="row">
+            	<div class="sixcol">qweqweqwe</div>
+                <div class="sixcol last">qweqwe</div>
+            </div>
+       </div>
+       
+       
+    
+    
+   
+    </body>
+</html>
