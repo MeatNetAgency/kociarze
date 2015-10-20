327f14eb7b7859de90e75bc3008432087bebcf96
Karol Sobolewski <karol@meatnet.pl>
2015-10-20 14:00:28 +0200

    zmiany do slidera i marginesu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+function resize()
+{
+	//resize main-menu row:
+	var height = 				$("#main-menu").outerHeight();
+	var width = 				$("#main-menu").width();
+	var marginRight = 		$("#main-menu").css("margin-right");
+	var spacerWidth = $("#main-menu").parent().width() - ($("#main-menu").width() + $("#search-box").width());
+	$("#menu-spacer").css("margin-left", (width-1)).css("width", spacerWidth);
+	var position = $("#main-menu").position();
+	var lenght = position.left;
+	$("#row-spacer").css("width", lenght);
+	
+	
+	// set-up menu margin
+	var sliderHeight = $("#main-slider").height();
+	var menuHeight = $("#header").height();
+	var newMargin = sliderHeight - menuHeight;
+	
+	//alert("sliderHeight = "+sliderHeight + "\n" + "menuHeight = " + menuHeight + "\n" + "newMargin = "+newMargin);
+	
+	$("#header").css("margin-bottom", newMargin);
+}
+
+
+
+
+
+function repositionSlider()
+{
+	// set up slider
+	$("#main-slider").css("position", "absolute").css("top", 0).css("z-index", "-2");
+}
+
+
 $(document).ready(function()
 {
 	
@@ -9,9 +43,7 @@ $(document).ready(function()
 	
 	
 	
-	
-	// set up slider
-	$("#main-slider").css("position", "absolute").css("top", 0).css("z-index", "-2");
+	repositionSlider();
 	
 	
 	
@@ -20,31 +52,16 @@ $(document).ready(function()
 	
 	
 	
-	//resize main-menu row:
-	var height = 				$("#main-menu").outerHeight();
-	var width = 				$("#main-menu").width();
-	var marginRight = 		$("#main-menu").css("margin-right");
 	
-	var spacerWidth = $("#main-menu").parent().width() - ($("#main-menu").width() + $("#search-box").width());
 	
-	//alert($("#main-menu").width() + "\n" + $("#search-box").width() + "\n" + $("#main-menu").parent().width());
+	setTimeout("resize()", 100);
 	
-	$("#menu-spacer").css("margin-left", (width-1)).css("width", spacerWidth);
 	
-	var position = $("#main-menu").position();
-	var lenght = position.left;
-	$("#row-spacer").css("width", lenght);
 	
+	
+	//resize main-menu row on screen change
 	$( window ).resize(function() {
-		var height = 				$("#main-menu").outerHeight();
-		var width = 				$("#main-menu").width();
-		var marginRight = 		$("#main-menu").css("margin-right");
-		var spacerWidth = $("#main-menu").parent().width() - ($("#main-menu").width() + $("#search-box").width());
-		$("#menu-spacer").css("margin-left", (width-1)).css("width", spacerWidth);
-		
-		var position = $("#main-menu").position();
-		var lenght = position.left;
-		$("#row-spacer").css("width", lenght);
+		resize();
 	});
 	
 	
