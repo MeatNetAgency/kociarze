84ab305d7107dfe82a12caaf8e57d306ed372785
Adam Kozlowski <Adam@iMac-Adam.local>
2015-10-20 12:21:15 +0200

    Made includes file: footer and header inside

---
+++ includes/footer.php
@@ -0,0 +1,59 @@
+﻿    <div class="container" id="footer">
+    	<div class="footer">
+        	<div class="row">
+            	<div class="threecol">
+                	<div class="footer-header">
+                    NAWIGACJA
+                    </div>
+                    <div class="footer-content">
+                    	<ul>
+                        	 <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="Strona główna">strona główna</a></li>
+                            <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="Aktualności">akutalności</a></li>
+                            <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="Koci magazyn">koci magazyn</a></li>
+                            <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="Wyzwanie">wyzwanie</a></li>
+                            <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="Forum">forum</a></li>
+                            <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="Blogi">blogi</a></li>
+                            <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="Klub">klub</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="threecol">
+                	<div class="footer-header">
+                    KONKURSY
+                    </div>
+                    <div class="footer-content">
+                    	<ul>
+                        	 <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="3 - tygodniowe wyzwanie">3 - tygodniowe wyzwanie</a></li>
+                            <li><span class="footer-arrows-color">&#x279d; &nbsp; &nbsp;</span><a href="#" title="Regulamin">regulamin</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="sixcol last">
+                	<div class="footer-header">
+                    O NAS
+                    </div>
+                    <div class="footer-content">
+                    W Purina rozumiemy jak wiele znaczy dla Ciebie Twój kot,<br/>
+					ponieważ sami jesteśmy właścicielami kotów.
+                    	<div class="row footer-graphics">
+                        	<div class="sixcol">
+                    			<a hover="#" title="Purina One"><img src="img/purina-logo-footer.png" alt="Purina: Twój ulubieniec -nasza pasja"/></a>
+                           </div>
+                           <div class="sixcol last more-info" >
+                           		<a href="#" title="Wicej informacji"><div class="button-comment teal-button-text">Wicej informacji &nbsp; &nbsp; &#x279d;</div></a>
+                           </div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row footer-bottom">
+            	<div class="threecol footer-end">© Copyright 2012-2015 kociarze.pl</div>
+                <div class="threecol footer-end"></div>
+                <div class="twocol footer-end"><a href="#" title="Kontakt">Kontakt</a></div>
+                <div class="twocol footer-end"><a href="#" title="Polityka prywatności">Polityka prywatności</a></div>
+                <div class="twocol last footer-end"><a href="#" title="Regulamin serwisu">Regulamin serwisu</a></div>
+            </div>
+    	</div>
+    </div>
+    </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,132 @@
+﻿<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Kociarze.pl</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!--JS JQUERY-->
+        <script src="js/jquery-1.11.3.min.js"></script>
+        
+        <!--JS JQUERY UI-->
+        <script src="js/jquery-ui/jquery-ui.min.js"></script>
+        
+        <!--JS FLEXSLIDER2-->
+        <script src="js/flexslider/jquery.flexslider-min.js"></script>
+
+        <!--MOSAIC FLOW PHOTO-->
+        <!--<script src="js/jquery.mosaicflow.min.js"></script>-->
+        
+        <!--JS MAIN-->
+        <script src="js/main.js"></script>
+            
+        <!--CSS GRID 1280 PX-->
+        <link rel="Stylesheet" type="text/css" href="css/grid.css" />
+        
+        <!-- GOOGLE FONTS: LATO -->
+        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,100&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        
+        <!--CSS FLEXSLIDER2-->
+        <link rel="Stylesheet" type="text/css" href="js/flexslider/flexslider.css" />
+        
+        <!--CSS JQUERY UI-->
+        <link rel="Stylesheet" type="text/css" href="js/jquery-ui/jquery-ui.min.css" />
+        
+        <!-- MAIN CSS -->
+        <link rel="Stylesheet" type="text/css" href="css/main.css" />
+
+	</head>
+        
+	<body>
+    
+    
+    
+    
+    
+    <!--SLIDER-->
+        <div id="main-slider">
+            <div class="flexslider">
+                <ul class="slides">
+                
+                	<!--FIRST SLIDER-->
+                    <li><img src="img/layer-main.png" alt="slide1" /></li>
+                    
+                    <!--SECOND SLIDER-->
+                    <li><img src="img/layer-main.png" alt="slide2" /></li>  
+                </ul>
+            </div>
+		</div>
+    
+    
+    
+    
+    
+    
+    
+    
+    <!-- HEADER:begin -->
+    <div class="container" id="header">
+    	
+        
+        <!-- Logos:begin -->
+        <div class="row" id="logotypes">
+            <div class="ninecol">
+            	<div class="row">
+                	<div class="threecol">
+                    	<div id="purinaone-logo"><a href="index.php" title="Purina One"><img class="logo-purina" src="img/logo-purina-one.png" alt="Purina One"/></a></div>
+                     </div>
+                    <div class="ninecol last">&nbsp;</div>
+                </div>
+            </div>
+            <div class="threecol last">
+            	<div  id="kociarze-logo">
+                	<a href="index.php" title="Kociarze.pl"><img class="logo-kociarze" src="img/logo-kociarze.png" alt="Kociarze.pl"/></a>
+                </div>   
+            </div>           
+        </div>
+        <!-- Logos:end -->
+        
+        
+        <!-- Main menu:begin -->
+        
+        <div class="row">
+        	<div id="row-spacer">&nbsp;</div>
+            <div id="nav-border-top">&nbsp;</div>
+            <div id="nav-border-bottom">&nbsp;</div>
+            <div class="ninecol" id="main-menu">
+                <nav>
+                    <ul>
+                        <li><a href="#" title="Strona główna"><img  src="img/ikona-home.png" alt="Aktualności" /></a></li>
+                        <li><a href="#" title="Aktualności" style="display:inline-block; padding-bottom:5px;">aktualności</a></li>
+                        <li><a href="#" title="Koci magazyn">koci magazyn</a></li>
+                        <li><a href="#" title="Wyzwanie">wyzwanie</a></li>
+                        <li><a href="#" title="Forum">forum</a></li>
+                        <li><a href="#" title="Blogi">blogi</a></li>
+                        <li><a href="#" title="Klub">klub</a></li>
+                        <li><a href="#" title="Produkty">produkty</a></li>
+                    </ul> 
+                </nav>
+            </div>
+            <div id="menu-spacer">&nbsp;</div>
+            <div class="threecol last" id="search-box">
+            	<div id="search"><a><input type="text" placeholder="Szukaj na stronie"/></a></div>
+            </div>
+            
+        </div>
+        <!-- Main menu:end -->
+        
+        
+        <!-- Login:begin -->
+        <div class="row">
+        	<div class="ninecol">&nbsp;</div>
+            <div class="threecol last">
+                <div id="login-link">
+                	<a href="#" title="Zaloguj się"><img src="img/ikona-person.png" alt="Zaloguj się" /> zaloguj się &nbsp; &nbsp; &nbsp; &nbsp;&#x2192;</a>
+                </div>        
+            </div>
+        </div>
+        <!-- Login:end -->
+        
+        
+    </div>
+    <!-- HEADER:end -->
