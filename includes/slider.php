8999a241d92b6145d46b0000472f145cb66b4210
Adam Kozlowski <Adam@iMac-Adam.local>
2015-10-27 10:16:50 +0100

    slider w katalogu incudes

---
+++ includes/slider.php
@@ -0,0 +1,36 @@
+		
+        
+        
+        <div class="container" style="margin:0px; padding:0px;">
+        <div id="main-slider">
+            <div class="flexslider">
+                <ul class="slides">
+                
+                	<!--FIRST SLIDER-->
+                    <li>
+                    	<img src="img/layer-main.png" alt="slide1" />
+                        
+                        <div class="flex-caption">
+                        	<h1>Dołącz do klubu Kociarza!</h1>
+                            <h2>Klub Kociarze - nowe miejsce na mapie naszego serwisu.<br>Dołącz do nas, na pierwszych 400 klubowiczów<br>czeka upominek.</h2>
+                        	<br>
+                            <a class="button button-teal" href="#" title="Dołącz" >Dołącz &nbsp; &nbsp; &#x279d;</a>
+                    		<br class="clear">
+                        </div>
+                    </li>
+                    
+                    <!--SECOND SLIDER-->
+                    <li>
+                    	<img src="img/layer-main.png" alt="slide2" />  
+                    	<div class="flex-caption">
+                        	<h1>Dołącz do klubu Kociarza!</h1>
+                            <h2>Klub Kociarze - nowe miejsce na mapie naszego serwisu.<br>Dołącz do nas, na pierwszych 400 klubowiczów<br>czeka upominek.</h2>
+                        	<br>
+                            <a class="button button-teal" href="#" title="Dołącz">Dołącz &nbsp; &nbsp; &#x279d;</a>
+                    		<br class="clear">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+		</div>
+        </div>
