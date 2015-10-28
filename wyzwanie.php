1d2c6a866d52de5de2269932edfbd83eb70c1ba9
Adam Kozlowski <Adam@iMac-Adam.local>
2015-10-28 12:20:40 +0100

    wyzwanie prace wstepne

--- wyzwanie.php
+++ wyzwanie.php
@@ -2,21 +2,21 @@
 	<?php include 'includes/header.php'; ?>
     <!-- HEADER:end -->
     
-    <div class="row">
-    	<div class="twelvecol">
-        	<div class="challenge" style="width:20%">
-            </div>
-            <div class="challenge" style="width:20%">
-            </div>
-            <div class="challenge" style="width:20%">
-            </div>
-            <div class="challenge" style="width:20%">
-            </div>
-            <div class="challenge" style="width:20%">
-            </div>
-        		
-        </div>
+
+    <!-- SLIDER:begin -->
+	<?php include 'includes/slider.php'; ?>
+    <!-- SLIDER:end -->
+    
+    
+  	<div class="chall" >
+    	<img src="img/wyzwanie-kot-2.jpg" />
+        <div class="chall-big-img">Testuj produkty PURINA ONE <br/> przez 3 tygodnie</div>
     </div>
+    
+    
+    <!-- FOOTER:begin -->
+	<?php include 'includes/footer.php'; ?>
+    <!-- FOOTER:end -->
     
 
     
