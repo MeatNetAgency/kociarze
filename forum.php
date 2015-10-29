6d264df903b83d3c4bd8d46fa1009deb7771c770
Adam Kozlowski <Adam@iMac-Adam.local>
2015-10-29 16:28:21 +0100

    Forum

---
+++ forum.php
@@ -0,0 +1,163 @@
+﻿    <!-- HEADER:begin -->
+	<?php include 'includes/header.php'; ?>
+    <!-- HEADER:end -->
+    
+    
+    <!-- SUBSLIDER:begin -->
+	<?php include 'includes/subslider.php'; ?>
+    <!-- SUBSLIDER:end -->
+    
+    
+	<div class="container">
+   		
+        <div class="row">
+        
+        	<!-- SUBMENU:begin -->
+            
+        	<div class="threecol">
+            	<ul id="sub-nav">
+                        <li class="parent">
+                        	<a href="javascript:void();" title="Tytuł strony" class="active">2015</a>
+                            <ul>
+                            	<li><a href="#" title="Tytuł strony" class="active">Grudzień</a></li>
+                                <li><a href="#" title="Tytuł strony">Listopad</a></li>
+                                <li><a href="#" title="Tytuł strony">Październik</a></li>
+                                <li><a href="#" title="Tytuł strony">Wrzesień</a></li>
+                            </ul>
+                       	</li>
+                        <li class="parent">
+                        	<a href="javascript:void();" title="Tytuł strony">2014</a>
+                            <ul>
+                            	<li><a href="#" title="Tytuł strony">Grudzień</a></li>
+                                <li><a href="#" title="Tytuł strony">Listopad</a></li>
+                                <li><a href="#" title="Tytuł strony">Październik</a></li>
+                                <li><a href="#" title="Tytuł strony">Wrzesień</a></li>
+                            </ul>
+                       	</li>
+                        <li class="parent">
+                        	<a href="javascript:void();" title="Tytuł strony">2013</a>
+                            <ul>
+                            	<li><a href="#" title="Tytuł strony">Grudzień</a></li>
+                                <li><a href="#" title="Tytuł strony">Listopad</a></li>
+                                <li><a href="#" title="Tytuł strony">Październik</a></li>
+                                <li><a href="#" title="Tytuł strony">Wrzesień</a></li>
+                            </ul>
+                       	</li>
+                </ul>
+            </div>
+            
+            <!-- SUBMENU:end -->
+            
+             <div class="ninecol last">
+                    
+                     <!-- FORUM DASHBOARD:begin -->
+
+                    
+                    
+                    	<div class="row">
+            				<div class="twelvecol">
+            					<h2 class="fleft title">Forum</h2>
+            					<div class="arrow-nav fright">
+                					<a href="#" class="margin"><span class="toback">&lt;</span></a><a href="#" class="margin active"><span class="number">1</span></a><a  href="#" class="margin"><span class="number">2</span></a><a  href="#" class="margin"><span class="number">3</span></a><a  href="#" class="margin"><span class="number">...</span></a><a  href="#" class="margin"><span class="number">324</span></a><a href="#"><span class="toforward ">&gt;</span></a>
+                				</div>
+            				</div>
+        				</div>
+        
+        
+        				<div class="row">
+        					<div class="twelvecol">
+            					<article class="forum">
+                					<div class="row">
+                                    
+                    					<div class="fourcol">
+                        					<div class="forum-padding">
+                        					<div class="right-border">
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                                <div style="clear:both;"></div>
+                                				<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                                <div style="clear:both;"></div>
+                                				<h4> Duis aute irure dolor in reprehenderit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                                <div style="clear:both;"></div>
+                                				<h4> Excepteur sint occaecat cupidatat non proident,</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                            				</div>
+                            				</div>
+                        				</div>
+                                        
+                                        
+                                        <div class="fourcol">
+                        					<div class="forum-padding">
+                        					<div class="right-border">
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                                <div style="clear:both;"></div>
+                                				<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                                <div style="clear:both;"></div>
+                                				<h4> Duis aute irure dolor in reprehenderit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                               <div style="clear:both;"></div>
+                                				<h4> Excepteur sint occaecat cupidatat non proident,</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                            				</div>
+                            				</div>
+                        				</div>
+                                        
+                                        
+                                        <div class="fourcol last">
+                        					<div class="forum-padding">
+                        					<div class="no-border">
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                                <div style="clear:both;"></div>
+                                				<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                                <div style="clear:both;"></div>
+                                				<h4> Duis aute irure dolor in reprehenderit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015 &nbsp;&nbsp;|&nbsp;&nbsp; Autor: <a href="#" title="Autor" >Admin</a></div>
+                                               <div style="clear:both;"></div>
+                                				<h4> Excepteur sint occaecat cupidatat non proident,</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                                	 <a class="button button-teal " href="#" title="Dodaj nowy wątek">Dodaj nowy wątek &nbsp; &nbsp; &#x279d;</a>
+                    								<br class="clear">
+                            				</div>
+                            				</div>
+                        				</div>
+
+                    				</div>
+                			</article>
+   
+                 
+                 </div>
+               	</div>
+                 <!-- FORUM DASHBOARD:end -->
+                 
+                 <!-- WYSIWIG:begin -->
+                 <div>
+                 	EDYTOR TEKSTU DO WPISOW
+                 </div>
+                 <!-- WYSIWIG:end -->
+
+    
+            </div>
+        </div>
+  
+    </div>
+    
+    
+        
+    <!-- FOOTER:begin -->
+	<?php include 'includes/footer.php'; ?>
+    <!-- FOOTER:end -->
