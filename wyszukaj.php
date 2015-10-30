49566c3386e6e29fd3374642b7a888b2134ee090
Adam Kozlowski <Adam@iMac-Adam.local>
2015-10-30 11:52:53 +0100

    Wyszukiwarka

---
+++ wyszukaj.php
@@ -0,0 +1,214 @@
+﻿	<?php include 'includes/header.php'; ?>
+    <!-- HEADER:end -->
+    
+    
+
+    
+    <!-- SLIDER:begin -->
+	<?php include 'includes/slider.php'; ?>
+    <!-- SLIDER:end -->
+    
+    
+    
+    
+    
+    
+    
+    <!-- WYSZUKANI UZYTKOWNICY:begin -->
+    
+    	 <div class="row">
+        	<div class="twelvecol">
+            	<h1 class="subtitle">Użytkownicy</h1>
+        	</div>
+		</div>
+    	<div class="row">
+        	<div class="threecol">
+				<div class="photo-gallery seeker-user friends">
+				
+                <!-- small photos:begin -->
+                <div class="gallery-small-photo-sub seeker-user-photo" >
+                	<img src="img/koci-magazyn-image.png" alt="Nazwa kota" />	<!-- BACKGROUND -->
+                    <div class="photo-background-s"></div>	
+                    <div class="photo-des-content-s ">
+                    	<div class="photo-text-padding-s">
+                       <div class="cats-name-s-left"><a href="#" title="Tytuł artykułu">User</a></div>
+                       <div class="photo-data-s-left"><a href="profil.php" title="Nazwa uzytkownika">zobacz profil</a></div>
+                       <div class="button-white"> Zaproś do znajomych</div>
+       
+                    </div>
+                     </div>
+                </div>
+ 				 <div style="clear:both"></div>
+                 <!-- small photos:end -->
+                 
+                 </div> 
+            </div>
+            
+        	<div class="threecol">
+				<div class="photo-gallery seeker-user friends">
+				
+                <!-- small photos:begin -->
+                <div class="gallery-small-photo-sub seeker-user-photo" >
+                	<img src="img/koci-magazyn-image.png" alt="Nazwa kota" />	<!-- BACKGROUND -->
+                    <div class="photo-background-s"></div>	
+                    <div class="photo-des-content-s ">
+                    	<div class="photo-text-padding-s">
+                       <div class="cats-name-s-left"><a href="#" title="Tytuł artykułu">User</a></div>
+                       <div class="photo-data-s-left"><a href="profil.php" title="Nazwa uzytkownika">zobacz profil</a></div>
+                       <div class="button-white"> Zaproś do znajomych</div>
+       
+                    </div>
+                     </div>
+                </div>
+ 				 <div style="clear:both"></div>
+                 <!-- small photos:end -->
+                 
+                 </div> 
+            </div>
+            
+            
+            
+            <div class="threecol"></div>
+            <div class="threecol last"></div>
+        </div>
+
+    <!-- WYSZUKANI UZYTKOWNICY:end -->
+    
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <br/><br/>
+    
+    
+    
+    
+    <!-- WYSZUKANE ARTYKUŁY:begin -->
+        <div class="row">
+        	<div class="twelvecol">
+            	<h1 class="subtitle">Artykuły</h1>
+        	</div>
+		</div>
+        
+        <div class="row seeker-article">
+        	<div class="onecol">
+            	<div><img src="img/koci-magazyn-image.png" alt="Nazwa kota"/></div>
+            </div>
+            <div class="elevencol last">
+                <div><h3><a href="#" alt="Tytuł">Lorem ipsum</a><h3></div>
+                <div class="author fleft">Autor: <a href="#" title="Autor" >Admin </a></div>
+                <div class="date"> Data: 22.05.2015</div>
+            </div>
+		</div>
+        
+         <div class="row seeker-article">
+        	<div class="onecol">
+            	<div><img src="img/koci-magazyn-image.png" alt="Nazwa kota"/></div>
+            </div>
+            <div class="elevencol last">
+                <div><h3><a href="#" alt="Tytuł">Lorem ipsum</a><h3></div>
+                <div class="author fleft">Autor: <a href="#" title="Autor" >Admin </a></div>
+                <div class="date"> Data: 22.05.2015</div>
+            </div>
+		</div>
+        
+          <div class="row seeker-article">
+        	<div class="onecol">
+            	<div><img src="img/koci-magazyn-image.png" alt="Nazwa kota"/></div>
+            </div>
+            <div class="elevencol last">
+                <div><h3><a href="#" alt="Tytuł">Lorem ipsum</a><h3></div>
+                <div class="author fleft">Autor: <a href="#" title="Autor" >Admin </a></div>
+                <div class="date"> Data: 22.05.2015</div>
+            </div>
+		</div>
+   
+    <!-- WYSZUKANE ARTYKUŁY:end -->
+    
+    
+    
+        <br/><br/>
+        
+        
+    
+    
+    <!-- WYSZUKANE BLOGI:begin -->
+       <div class="row">
+        	<div class="twelvecol">
+            	<h1 class="subtitle">Blogi</h1>
+        	</div>
+		</div>
+        
+        
+        <div class="row seeker-article">
+        	<div class="onecol">
+            	<div><img src="img/koci-magazyn-image.png" alt="Nazwa kota"/></div>
+            </div>
+            <div class="elevencol last">
+                <div><h3><a href="#" alt="Tytuł">Lorem ipsum</a><h3></div>
+                <div class="author fleft">Autor: <a href="#" title="Autor" >Admin </a></div>
+                <div class="date"> Data: 22.05.2015</div>
+            </div>
+		</div>
+    <!-- WYSZUKANE BLOGI:end -->
+    
+    
+       <br/><br/>
+    
+    
+    <!-- WYSZUKANE WPISY FORUM:begin -->
+       <div class="row">
+        	<div class="twelvecol">
+            	<h1 class="subtitle">Forum</h1>
+        	</div>
+		</div>
+        
+        <div class="row seeker-article">
+            <div class="twelvecol last">
+                <div><h3><a href="#" alt="Tytuł">Lorem ipsum</a><h3></div>
+                <div class="author fleft">Autor: <a href="#" title="Autor" >Admin </a></div>
+                <div class="date"> Data: 22.05.2015</div>
+            </div>
+		</div>
+    <!-- WYSZUKANE WPISY FORUM:end -->
+
+    
+    
+    
+    
+    
+    
+
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <!-- FOOTER:begin -->
+	<?php include 'includes/footer.php'; ?>
+    <!-- FOOTER:end -->
