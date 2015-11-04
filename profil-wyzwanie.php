1dfa4160d10fb0ed310cb268530e8f4cf493d332
Adam Kozlowski <Adam@iMac-Adam.local>
2015-11-04 15:40:15 +0100

    profil-wyzwanie

---
+++ profil-wyzwanie.php
@@ -0,0 +1,92 @@
+﻿    <!-- HEADER:begin -->
+	<?php include 'includes/header.php'; ?>
+    <!-- HEADER:end -->
+    
+    
+    
+    
+    
+    
+    <!-- SLIDER:begin -->
+	<?php include 'includes/subslider.php'; ?>
+    <!-- SLIDER:end -->
+    
+	<br>
+        
+    
+	<div class="container" id="content">
+    
+    	<div class="row">
+            	
+        	<div class="threecol">
+            	<ul id="sub-nav">
+                    	<li><a href="#" title="Mój profil">Mój profil</a></li>
+                        <li><a href="#" title="Wiadomości">Wiadomości</a></li>
+                        <li><a href="#" title="Znajomi" >Znajomi</a></li>
+                        <li><a href="#" title="Wyzwanie" class="active">Wyzwanie</a></li>
+                </ul>
+            </div>
+            
+            <div class="ninecol last user">
+            	<div class="row">
+                	<div><p><h2>Moje koty - kliknij na zdjecie, aby podjąć wyzwanie</h2></p></div>
+                    <div class="row">
+                    
+        				<div class="threecol fleft">
+            				<a href="#" title="Imie kota"><img src="img/kot-maly.png" alt="Nazwa użytkownika"/></a>
+                            <div class="challenge-started">
+                          		<div><a href="#" title="Nazwa kota"><p><h1>Amber</h1></p></a></div>
+                            	<div><h3>Kot rozpoczął wyzwanie:<span class="profile-info"> 11.23.2013</span></h3></div>
+                        	</div>
+            			</div>
+                        
+                        <div class="threecol fleft">
+            				<a href="#" title="Imie kota"><img src="img/kot-maly.png" alt="Nazwa użytkownika"/></a>
+                            <div class="challenge-started">
+                          		<div><a href="#" title="Nazwa kota"><p><h1>Bamber</h1></p></a></div>
+                            	<div><h3>Kot rozpoczął wyzwanie:<span class="profile-info"> 11.23.2013</span></h3></div>
+                        	</div>
+            			</div>
+                        
+                        <div class="threecol fleft">
+            				<a href="#" title="Imie kota"><img src="img/kot-maly.png" alt="Nazwa użytkownika"/></a>
+                            <div>
+                            	<div><a href="#" title="Nazwa kota"><p><h1>Burek</h1></p></a></div>
+                        	</div>
+            			</div>
+                        
+                        <div class="threecol fleft last">
+            				<a href="#" title="Imie kota"><img src="img/kot-maly.png" alt="Nazwa użytkownika"/></a>
+                            <div>
+                            	<div><a href="#" title="Nazwa kota"><p><h1>Koteł</h1></p></a></div>
+                        	</div>
+            			</div>
+                        
+        			</div>
+                </div>
+            	
+            </div>
+    	</div>
+        
+        
+        
+        
+        
+   </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    <!-- FOOTER:begin -->
+	<?php include 'includes/footer.php'; ?>
+    <!-- FOOTER:end -->
+    
+    
+	
