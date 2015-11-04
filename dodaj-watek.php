252e291a19b130f75a390f22b945c36c3714c0f7
Adam Kozlowski <Adam@iMac-Adam.local>
2015-11-04 12:43:59 +0100

    dodaj watek

---
+++ dodaj-watek.php
@@ -0,0 +1,47 @@
+﻿    <!-- HEADER:begin -->
+	<?php include 'includes/header.php'; ?>
+    <!-- HEADER:end -->
+    
+    
+    <!-- SUBSLIDER:begin -->
+	<?php include 'includes/subslider.php'; ?>
+    <!-- SUBSLIDER:end -->
+    
+    
+	<!-- ARTICLE:begin -->
+    <div class="container">
+    
+    	<div class="row">
+            <div class="twelvecol">
+            	<h2 class="fleft title">Dodaj nowy wątek</h2>
+            </div>
+        </div>
+    
+    	<div class="row">
+        	<div class="twelvecol">
+            	<article class="form">
+                	<div class="row">
+                    	<div class="fourcol"><img class="photo desktop" src="img/pani-z-kotem.png" alt="Tytuł wpisu" /></div>
+                        <div class="eightcol last">
+                        	<img class="photo mobile" src="img/pani-z-kotem.png" alt="Tytuł wpisu" />
+                        	<h3>Tytuł wątku:</h3>
+                            	<input type="text" name="title"/>
+                            <h3>Treść wątku:</h3>
+                            	Miejsce na edytor - szerokość 100% diva-a
+                         
+                           <br class="clear">
+                            <a class="button button-grey " type"submit" title="Dodaj wątek">Dodaj wątek &nbsp; &nbsp; &#x279d;</a>
+                    		
+                        </div>
+                    </div>
+                </article>
+            </div>
+     	</div>
+     </div>
+     <!-- ARTICLE:end -->
+    
+    
+        
+    <!-- FOOTER:begin -->
+	<?php include 'includes/footer.php'; ?>
+    <!-- FOOTER:end -->
