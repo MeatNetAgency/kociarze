efd066a96b59a984b36dabeb10150d7be680660a
Adam Kozlowski <Adam@iMac-Adam.local>
2015-11-04 16:34:28 +0100

    wydarzenia.php

---
+++ wydarzenia.php
@@ -0,0 +1,191 @@
+    <!-- HEADER:begin -->
+	<?php include 'includes/header.php'; ?>
+    <!-- HEADER:end -->
+    
+    
+    <!-- SUBSLIDER:begin -->
+	<?php include 'includes/subslider.php'; ?>
+    <!-- SUBSLIDER:end -->
+    
+    
+	<div class="container">
+   		
+        <div class="row">
+        
+        	<!-- SUBMENU:begin -->
+            
+			<div class="threecol">
+            
+            	<ul id="sub-nav">
+                    	<li><a href="#" title="Aktualności">Aktualności</a></li>
+                        <li><a href="#" title="Kocim okiem" >Kocim okiem</a></li>
+                        <li><a href="#" title="Porady eksperta">Porady eksperta</a></li>
+                        <li><a href="#" title="Wydarzenia"  class="active">Wydarzenia</a></li>
+                        <li><a href="#" title="Badania i rozwój">Badania i rozwój</a></li>
+                </ul>
+            
+            </div>
+            
+            <!-- SUBMENU:end -->
+            
+             <div class="ninecol last">
+            
+            <!-- NEWS:begin -->
+            	<div class="row">
+                	<div class="fourcol">
+                    	<h2 class="first-title">Wydarzenia</h2>
+                    </div>
+                    <div class="fourcol"><h2>&nbsp;</h2></div>
+                    <div class="fourcol last"><h2>&nbsp;</h2></div>
+                    
+
+                    <div class="fourcol">
+            			<article class="news">
+                			<img class="photo" src="img/image-kot-2.png" alt="Tytuł artykułu"/>
+                    		<h3>Wyniki konkursu 3-tygodniowe wyzwanie - widoczne zdrowie 2015!</h3>
+                   			<div class="author">Autor: <a href="#" title="Autor" >Admin</a></div>
+                    		<div class="date">Data: 22.05.2015</div>
+                    		<br class="clear">
+                    		<p>Drodzy Kociarze, gratulujemy wszystkim uczestnikom i laureatom konkursu towarzyszącemu 3-tygodniowemu wyzwaniu. Publikujemy długo...</p>
+                    		<a class="button button-grey" href="page.php"  title="Czytaj dalej" >Czytaj dalej &nbsp; &nbsp; &#x279d;</a>
+                    		<br class="clear">
+                		</article>
+                    </div>
+                    <div class="fourcol">
+                    	<article class="news">
+                			<img class="photo" src="img/image-kot-2.png" alt="Tytuł artykułu"/>
+                    		<h3>Wyniki konkursu 3-tygodniowe wyzwanie - widoczne zdrowie 2015!</h3>
+                   			<div class="author">Autor: <a href="#" title="Autor" >Admin</a></div>
+                    		<div class="date">Data: 22.05.2015</div>
+                    		<br class="clear">
+                    		<p>Drodzy Kociarze, gratulujemy wszystkim uczestnikom i laureatom konkursu towarzyszącemu 3-tygodniowemu wyzwaniu. Publikujemy długo...</p>
+                    		<a class="button button-grey " href="page.php"  title="Czytaj dalej" >Czytaj dalej &nbsp; &nbsp; &#x279d;</a>
+                    		<br class="clear">
+                		</article>
+                    </div>
+                    <div class="fourcol last">
+                    	<article class="news">
+                			<img class="photo" src="img/image-kot-2.png" alt="Tytuł artykułu"/>
+                    		<h3>Wyniki konkursu 3-tygodniowe wyzwanie - widoczne zdrowie 2015!</h3>
+                   			<div class="author">Autor: <a href="#" title="Autor" >Admin</a></div>
+                    		<div class="date">Data: 22.05.2015</div>
+                    		<br class="clear">
+                    		<p>Drodzy Kociarze, gratulujemy wszystkim uczestnikom i laureatom konkursu towarzyszącemu 3-tygodniowemu wyzwaniu. Publikujemy długo...</p>
+                    		<a class="button button-grey"  href="page.php"  title="Czytaj dalej" >Czytaj dalej &nbsp; &nbsp; &#x279d;</a>
+                    		<br class="clear">
+                		</article>
+                    </div>
+              	</div>
+             <!-- NEWS:end -->
+                    
+                    
+                    
+                    
+        
+                    <br/><br/>
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                     <!-- FORUM DASHBOARD:begin -->
+                 <div class="row">
+                    <div class="twelvecol">
+                    
+                    
+                    	<div class="row">
+            				<div class="twelvecol">
+            					<h2 class="fleft title">Historia</h2>
+            					<div class="arrow-nav fright">
+                					<a href="#" class="margin"><span class="toback">&lt;</span></a><a href="#" class="margin active"><span class="number">1</span></a><a  href="#" class="margin"><span class="number">2</span></a><a  href="#" class="margin"><span class="number">3</span></a><a  href="#" class="margin"><span class="number">...</span></a><a  href="#" class="margin"><span class="number">324</span></a><a href="#"><span class="toforward ">&gt;</span></a>
+                				</div>
+            				</div>
+        				</div>
+        
+        
+        				<div class="row">
+        					<div class="twelvecol">
+            					<article class="forum">
+                					<div class="row">
+                                    
+                    					<div class="fourcol">
+                        					<div class="forum-padding">
+                        					<div class="right-border">
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4> Duis aute irure dolor in reprehenderit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4> Excepteur sint occaecat cupidatat non proident,</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                            				</div>
+                            				</div>
+                        				</div>
+                                        
+                                        
+                                        <div class="fourcol">
+                        					<div class="forum-padding">
+                        					<div class="right-border">
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4> Duis aute irure dolor in reprehenderit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4> Excepteur sint occaecat cupidatat non proident,</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                            				</div>
+                            				</div>
+                        				</div>
+                                        
+                                        
+                                        <div class="fourcol last">
+                        					<div class="forum-padding">
+                        					<div class="no-border">
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4> Duis aute irure dolor in reprehenderit</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                
+                                				<div class="date">Data: 22.05.2015</div>
+                                				<h4> Excepteur sint occaecat cupidatat non proident,</h4>
+                                				<div class="replies"><a href="#" title="Odpowiedzi" >5 odpowiedzi</a></div>
+                                                	 <a class="button button-teal " href="#" title="Dodaj nowy wątek">Dodaj nowy wątek &nbsp; &nbsp; &#x279d;</a>
+                    								<br class="clear">
+                            				</div>
+                            				</div>
+                        				</div>
+
+                    				</div>
+                			</article>
+           				 </div>
+     				</div>
+                 
+                 </div>
+               	</div>
+                 <!-- FORUM DASHBOARD:end -->
+                 
+
+    
+            </div>
+        </div>
+  
+    </div>
+    
+    
+        
+    <!-- FOOTER:begin -->
+	<?php include 'includes/footer.php'; ?>
+    <!-- FOOTER:end -->
